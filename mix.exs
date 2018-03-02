5662b07de166a222bc6493626115a25465062bc6
Chris McGrath <chris@chrismcg.com>
2018-03-02 22:08:56 +0000

    Failing config

--- mix.exs
+++ mix.exs
@@ -7,7 +7,8 @@ defmodule Diatest.MixProject do
       version: "0.1.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      dialyzer_warnings: [:unmatched_returns, :error_handling, :race_conditions, :unknown]
     ]
   end
 
@@ -21,6 +22,7 @@ defmodule Diatest.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:dialyzex, "~> 1.1.0", only: :dev}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
